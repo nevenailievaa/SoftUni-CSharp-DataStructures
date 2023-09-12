528ec82e631de8750a05eb59b6707c807e7343ff
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-09-12 16:21:41 +0300

    Add files via upload

---
+++ 01. Data Structures Fundamentals/02. Linear Data Structures - Exercise/04. Balanced Parentheses/BalancedParenthesesSolve.cs
@@ -0,0 +1,51 @@
+﻿namespace Problem04.BalancedParentheses
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection.Metadata.Ecma335;
+
+    public class BalancedParenthesesSolve : ISolvable
+    {
+        public bool AreBalanced(string parentheses)
+        {
+            if (parentheses.Length % 2 == 1)
+            {
+                return false;
+            }
+
+            var stack = new Stack<char>(parentheses.Length / 2 );
+
+            foreach (var currentChar in parentheses)
+            {
+                char expectedChar = default;
+                switch (currentChar)
+                {
+                    case ')':
+                        expectedChar = '(';
+                        break;
+                    case ']':
+                        expectedChar = '[';
+                        break;
+                    case '}':
+                        expectedChar = '{';
+                        break;
+                    default:
+                        stack.Push(currentChar);
+                        break;
+                }
+
+                if (expectedChar == default)
+                {
+                    continue;
+                }
+
+                if (stack.Pop() != expectedChar)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
