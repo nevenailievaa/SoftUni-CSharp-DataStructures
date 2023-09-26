e61e474c245f7e2ba10aa3ac5723c4c74ee21f7e
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-09-27 00:21:38 +0300

    Add files via upload

---
+++ 01. Data Structures Fundamentals/05. Heaps, BST/03. Max Heap/IAbstractHeap.cs
@@ -0,0 +1,18 @@
+﻿namespace _03.MaxHeap
+{
+    using System;
+
+    public interface IAbstractHeap<T> 
+        where T : IComparable<T>
+    {
+        //Properties
+        int Size { get; }
+
+        //Methods
+        void Add(T element);
+
+        T Peek();
+
+        T ExtractMax();
+    }
+}
---
+++ 01. Data Structures Fundamentals/05. Heaps, BST/03. Max Heap/MaxHeap.cs
@@ -0,0 +1,109 @@
+﻿namespace _03.MaxHeap
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class MaxHeap<T> : IAbstractHeap<T> where T : IComparable<T>
+    {
+        //Fields
+        private List<T> elements;
+
+        //Constructor
+        public MaxHeap()
+        {
+            elements = new List<T>();
+        }
+
+        //Properties
+        public int Size => elements.Count;
+
+        //Methods
+        public void Add(T element)
+        {
+            elements.Add(element);
+            HeapifyUp(elements.Count - 1);
+        }
+
+        private void HeapifyUp(int index)
+        {
+            var parentIndex = GetParentIndex(index);
+
+            while (index > 0 && IsGreater(index, parentIndex))
+            {
+                Swap(index, parentIndex);
+                index = parentIndex;
+                parentIndex = GetParentIndex(index);
+            }
+        }
+
+        private bool IsGreater(int index, int parentIndex) => elements[index].CompareTo(elements[parentIndex]) > 0;
+
+        private void Swap(int index, int parentIndex)
+        {
+            var temp = elements[index];
+            elements[index] = elements[parentIndex];
+            elements[parentIndex] = temp;
+        }
+
+        private int GetParentIndex(int index) => (index - 1) / 2;
+
+        public T ExtractMax()
+        {
+            if (Size == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            T element = elements[0];
+            Swap(0, elements.Count - 1);
+            elements.RemoveAt(Size - 1);
+            HeapifyDown(0);
+
+            return element;
+        }
+        private void HeapifyDown(int index)
+        {
+            var biggerChildIndex = GetBiggerChildIndex(index);
+
+            while (IsIndexValid(biggerChildIndex) && IsGreater(biggerChildIndex, index))
+            {
+                Swap(biggerChildIndex, index);
+                biggerChildIndex = GetBiggerChildIndex(index);
+            }
+        }
+
+        private bool IsIndexValid(int index) => index >= 0 && index < Size;
+
+        private int GetBiggerChildIndex(int index)
+        {
+            var firstChildIndex = index * 2 + 1;
+            var secondChildIndex = index * 2 + 2;
+
+            if (secondChildIndex < Size)
+            {
+                if (IsGreater(firstChildIndex, secondChildIndex))
+                {
+                    return firstChildIndex;
+                }
+                return secondChildIndex;
+            }
+            else if (firstChildIndex < Size)
+            {
+                return firstChildIndex;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public T Peek()
+        {
+            if (elements.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            return elements[0];
+        }
+    }
+}
