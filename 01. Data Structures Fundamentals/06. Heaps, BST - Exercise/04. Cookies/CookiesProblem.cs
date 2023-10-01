16c0786eaa8acb61442ad1216141ffb0ed7ae8b1
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-10-01 16:35:21 +0300

    Add files via upload

---
+++ 01. Data Structures Fundamentals/06. Heaps, BST - Exercise/04. Cookies/CookiesProblem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using _03.MinHeap;
+using Wintellect.PowerCollections;
+
+namespace _04.CookiesProblem
+{
+    public class CookiesProblem
+    {
+        public int Solve(int minSweetness, int[] cookies)
+        {
+            OrderedBag<int> bag = new OrderedBag<int>();
+            bag.AddMany(cookies);
+
+            int steps = 0;
+            int currentSweetness = bag.GetFirst();
+
+            while(currentSweetness < minSweetness && bag.Count > 1)
+            {
+                int firstCookie = bag.RemoveFirst();
+                int secondCookie = bag.RemoveFirst();
+
+                int newCookie = firstCookie + 2 * secondCookie;
+                bag.Add(newCookie);
+                currentSweetness = bag.GetFirst();
+
+                steps++;
+            }
+
+            return currentSweetness < minSweetness ? - 1 : steps;
+        }
+    }
+}
